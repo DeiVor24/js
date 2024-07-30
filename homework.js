22350eb6a408ad4c0215e685340630a59006ac08
DeiVis <zbitkisas@gmail.com>
2024-07-30 23:55:05 +0300

    hw

---
+++ homework.js
@@ -0,0 +1,64 @@
+console.clear();
+
+// Kintamųjų inicijavimas
+
+// 1. Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+
+const a = 3;
+const b = 2;
+const c = 5;
+console.log(a, b, c);
+
+
+// 2. Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+
+const diena = 'pirmadienis';
+const diena1 = 'antrdienis';
+const diena2 = 'treciadienis';
+console.log(diena, diena1, diena2);
+
+// 3. Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+
+const speed = [100, 120, 240, 300, 20];
+const way = [1, 2, 3, 4, 5];
+const time = [10, 15, 5, 3, 4];
+console.log(speed, way, time);
+
+// 4. Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+
+
+const doctors = ['Jonas', 'Marius', 'Reda', 'Benas', 'Tadas'];
+const drivers = ['Sigis', 'Rytis', 'Inga', 'Eva', 'Ana'];
+const butchers = ['Gedas', 'Erika', 'Albinas', 'Kostas', 'Ugne'];
+
+console.log(doctors);
+console.log(drivers);
+console.log(butchers);
+
+// Veiksmai su kintamaisiais
+
+// 1. Susumuoti visus skaičiaus tipo kintamuosius
+// Rezultatą išvesti į console
+
+const suma = a + b +c;
+console.log(suma);
+
+// 2. Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
+// Rezultatą išvesti į console
+
+const dienos = 'pirmadienis '+'antradienis '+'treciadienis';
+console.log(dienos);
+
+// 3. Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
+// 1-2+3-4+5
+// Rezultatą išvesti į console
+
+
+
+ 
+
+
