d5597eab9a86f1e507c1e0be5c24d84288da4fcd
DeiVis <zbitkisas@gmail.com>
2024-08-07 11:03:35 +0300

    tofixed

---
+++ js/method/numbers.js
@@ -0,0 +1,17 @@
+console.log(isFinite(-5));
+console.log(isFinite(0));
+console.log(isFinite(5));
+console.log(isFinite(5.148));
+console.log(isFinite(-5.148));
+console.log(isFinite(NaN));
+console.log(isFinite(Infinity));
+console.log(isFinite(-Infinity));
+
+
+const n3 = 0.1;
+const n4 = 0.2;
+
+const n5 = n3 + n4;
+
+console.log(n5.toFixed(4));
+
